09268f96d2bad1ba05c011951d3074e0ed25d59e
Freeman Chen <pucelle@163.com>
2024-10-26 14:43:23 +0800

    Avoid multiple time component connection

--- src/component/component.ts
+++ src/component/component.ts
@@ -414,39 +414,27 @@ export class Component<E = any> extends EventFirer<E & ComponentEvents> implemen
 	
 	/** Append current element into a container, and do connect. */
 	appendTo(container: Element) {
-		if (this.connected) {
-			this.remove()
-		}
-		
 		container.append(this.el)
 		
-		if (document.contains(this.el)) {
+		if (!this.connected && document.contains(this.el)) {
 			this.afterConnectCallback(PartCallbackParameterMask.DirectNodeToMove)
 		}
 	}
 
 	/** Insert current element before an element, and do connect. */
 	insertBefore(sibling: Element) {
-		if (this.connected) {
-			this.remove()
-		}
-
 		sibling.before(this.el)
 
-		if (document.contains(this.el)) {
+		if (!this.connected && document.contains(this.el)) {
 			this.afterConnectCallback(PartCallbackParameterMask.DirectNodeToMove)
 		}
 	}
 
 	/** Insert current element after an element, and do connect. */
 	insertAfter(sibling: Element) {
-		if (this.connected) {
-			this.remove()
-		}
-
 		sibling.after(this.el)
 
-		if (document.contains(this.el)) {
+		if (!this.connected && document.contains(this.el)) {
 			this.afterConnectCallback(PartCallbackParameterMask.DirectNodeToMove)
 		}
 	}
